2b5058a7889f8830864ccff77ea3e966859561da
zapper-95 <zapper-95@github.com>
2022-08-30 23:25:30 +0100

    Neatened code by moving methods to static volume mixer class. Changed the method by which it is determined when to fade in or out and added another fade process. Memory seems to continously go upwards so this needs checking

--- Hush/Form1.cs
+++ Hush/Form1.cs
@@ -1,29 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Diagnostics;
-using System.Windows.Automation;
-using CSCore;
-using CSCore.CoreAudioAPI;
-using System.Runtime.InteropServices;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace Hush
 {
 
     public partial class Form1 : Form
     {
+        Thread controlMixThread = null;
         private ProcessInfo TargetProcess;
         private ProcessInfo SpeakerProcess;
         string SpeakerText;
         string TargetText;
-        int inertia_period = 3000;
+        int inertia_period = 0000;
         private List<ProcessInfo> processInfo = new List<ProcessInfo>();
         bool hush = false;
         public Form1()
@@ -36,17 +27,72 @@ namespace Hush
         public void RefreshProcesses()
         {
             processInfo.Clear();
-            Process[] processes = Process.GetProcesses();
             processInfo = VolumeMixer.GetProcessInfo();
-            foreach (var process in processInfo)
-            {
-                Console.WriteLine("Process ID: " + process.ID + " process name " + process.name);
-                //processInfo.Add(new ProcessInfo(process.Id, process.MainWindowTitle));
-            }
+
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //code here https://stackoverflow.com/questions/20938934/controlling-applications-volume-by-process-id
+            if (controlMixThread != null) //to not abort an empty thread (i.e the first time this is run)
+            {
+                controlMixThread.Abort();
+            }
 
+            TargetText = Target.Text;
+            SpeakerText = Speaker.Text;
+            hush = true;
+            controlMixThread = new Thread(ControlMixer);
+            controlMixThread.IsBackground = true;
+            controlMixThread.Start();
+
+        }
+        private void ControlMixer()
+        {
+            Thread thread = null;
+            int SpeakerID = 0, TargetID = 0;
+            SpeakerID = SpeakerProcess.ID;
+            TargetID = TargetProcess.ID;
+            float target_loud_volume = VolumeMixer.GetVolume(TargetID);
+            string oldMixerState = "";
+            while (hush && SpeakerProcess != null && TargetProcess != null)
+            {
+                //Console.WriteLine(target_loud_volume);
+
+
+
+                string newMixerState = VolumeMixer.FindMixerState(SpeakerID, TargetID);
+
+
+                if (oldMixerState != newMixerState) // we only want to fade if the state has changed
+                {
+                    Thread.Sleep(inertia_period);
+                    if (VolumeMixer.FindMixerState(SpeakerID, TargetID) == newMixerState) //if it is the same state after the inertia period seconds fade
+                    {
+                        if (thread != null && thread.IsAlive)
+                        {
+                            thread.Abort();
+                        }
+                        thread = new Thread(() => Fade(SpeakerID, TargetID, target_loud_volume, newMixerState));
+                        thread.IsBackground = true;
+                        thread.Start();
+                        oldMixerState = newMixerState;
+
+                    }
+
+
+                }
+
+
+
+
+
+
+
+            }
+
+        }
         private void DropDown(object sender, EventArgs e)
         {
             //code from https://stackoverflow.com/questions/4395405/how-can-i-obtain-a-list-of-currently-running-applications
@@ -68,234 +114,24 @@ namespace Hush
             }
         }
 
-        private void StartCounting()
+
+
+        private void Fade(int SpeakerID, int TargetID, float target_loud_volume, string newMixerState)
         {
-            int SpeakerID = 0, TargetID = 0;
-            float priorMasterVolume = 0;
-            string last_fade_state = "";
-            while (hush)
+            if(newMixerState == "FadeIn")
             {
-                if (SpeakerProcess != null && TargetProcess != null)
-                {
-                    SpeakerID = SpeakerProcess.ID;
-                    TargetID = TargetProcess.ID;
-                    try
-                    {
-                        Console.WriteLine(Process.GetProcessById(SpeakerID).ProcessName);
-                        if (Process.GetProcessById(SpeakerID).ProcessName == SpeakerText && Process.GetProcessById(TargetID).ProcessName==TargetText) //checks that the process with that number corresponds to the text in the dropdown
-                        {
-                            bool speaker_noise = VolumeMixer.ProcessPlayingAudio(SpeakerID);
-                            Console.WriteLine("Speaker noise " + speaker_noise);
-
-                            if (speaker_noise && last_fade_state != "out")
-                            {
-                                Thread.Sleep(inertia_period);
-                                if (VolumeMixer.ProcessPlayingAudio(SpeakerID))
-                                {
-                                    FadeOut(SpeakerID, TargetID, ref priorMasterVolume);
-                                    last_fade_state = "out";
-                                    Console.WriteLine("prior master volume " + priorMasterVolume);
-                                }
-
-                            }
-                            else if(!speaker_noise && last_fade_state != "in")
-                            {
-                                Thread.Sleep(inertia_period);
-                                if (!VolumeMixer.ProcessPlayingAudio(SpeakerID))
-                                {
-                                    FadeIn(SpeakerID, TargetID, priorMasterVolume);
-                                    Console.WriteLine("prior master volume " + priorMasterVolume);
-                                    last_fade_state = "in";
-                                }
-
-                            }
-                        }
-
-                    }
-                    catch (Exception)
-                    {
-
-                        Debug.WriteLine("Process does not exist");
-                    }
-                }
-
+                VolumeMixer.FadeIn(SpeakerID, TargetID, target_loud_volume);
             }
- 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //code here https://stackoverflow.com/questions/20938934/controlling-applications-volume-by-process-id
-            hush = true;
-            TargetText = Target.Text;
-            SpeakerText = Speaker.Text;
-            var thread = new Thread(StartCounting);
-            thread.IsBackground = true;
-            thread.Start();
-            if (hush)
+            else if(newMixerState == "FadeOut")
             {
-
-                //int SpeakerID = 0, TargetID = 0;
-
-                //if (SpeakerProcess != null && TargetProcess != null)
-                //{
-                //    SpeakerID = SpeakerProcess.ID;
-                //    TargetID = TargetProcess.ID;
-                //    try
-                //    {
-                //        if (Process.GetProcessById(SpeakerID).MainWindowTitle == Speaker.Text && Process.GetProcessById(TargetID).MainWindowTitle == Target.Text) //checks that the process with that number corresponds to the text in the dropdown
-                //        {
-                //            bool speaker_noise = VolumeMixer.ProcessPlayingAudio(SpeakerID);
-
-                //            if (speaker_noise)
-                //            {
-                //                FadeOut(SpeakerID, TargetID);
-                //            }
-                //        }
-
-                //    }
-                //    catch (Exception)
-                //    {
-
-                //        Debug.WriteLine("Process does not exist");
-                //    }
-                //}
+                VolumeMixer.FadeOut(SpeakerID, TargetID);
             }
-
-
-            
-            
-            
-  
-
-        }
-
-        private void FadeOut(int SpeakerID, int TargetID, ref float priorMasterVolume) //method to reduce volume exponentially
-        {
-            /*
-                                                            ----- EXPLANATION -----
-
-             The method used here to fade out volume is to at fixed intervals, reduce the master volume by an exponential function.
-
-                                     The exponential function grows by these time steps being its exponent
-            More specifically  y = r^n , where n is the current time step, and y is the value to be subtracted from the master volume
-
-
-            The sum of an exponential function from 1 to N is (1-r^N) / 1 - r
-            We can set this equation equal to the current master volume
-
-            Now we know after N time intervals, if we subtract r^n, we will eventually reduce the master volume to 0.
-
-            The variables r, and the total volume reduction (TVR) i.e the 'current master volume', are fixed, so we can calculate the number of time steps by rearanging the equation for N.
-
-            TVR = (1-r^N) / 1 - r  to make N the subject    N = (log(r-TVR(1-r)))/log(r) - 1
-
-            
-            The length of each time step will depend on how long we want the program to take to reduce the master volume to 0.
-            Let us call the time to reduce the master volume to 0, t.
-
-            t = alpha * N , where alpha is the length of each time step, and N is the number of time steps.
-
-            We can solve alpha be rearranging the equation.
-
-            */
-
-
-
-            float r = 1.01f; 
-            int t = 2;
-
-
-            float tvr = VolumeMixer.GetVolume(TargetID) * 100; //easier to work with values 0 to 100 and r values look nicer
-
-
-            priorMasterVolume = tvr;
-
-
-            //derrive N
-            int N = (int)Math.Floor((Math.Log(r - tvr * (1 - r)) / Math.Log(r)) - 1);
-
-            Console.WriteLine("n " + N);
-            //derrive alpha
-            decimal alpha = decimal.Divide(t, N);
-            Console.WriteLine("N " + N);
-            Console.WriteLine("alpha " + alpha);
-
-            //reduce the master volume
-            for (int i = 1; i <= N; i++)
-            {
-                Thread.Sleep((int)Math.Round(alpha * (decimal)Math.Pow(10, 3)));
-                float current_reduction = -(float)Math.Pow(r, i) / 100;
-                VolumeMixer.ChangeVolume(TargetID, current_reduction);
-
-
-
-
-            }
-
-
         }
 
 
-        private void FadeIn(int SpeakerID, int TargetID, float priorMasterVolume) //method to reduce volume exponentially
-        {
-            /*
-                                                            ----- EXPLANATION -----
-
-             The method used here to fade out volume is to at fixed intervals, reduce the master volume by an exponential function.
-
-                                     The exponential function grows by these time steps being its exponent
-            More specifically  y = r^n , where n is the current time step, and y is the value to be subtracted from the master volume
 
 
-            The sum of an exponential function from 1 to N is (1-r^N) / 1 - r
-            We can set this equation equal to the current master volume
-
-            Now we know after N time intervals, if we subtract r^n, we will eventually reduce the master volume to 0.
-
-            The variables r, and the total volume reduction (TVR) i.e the 'current master volume', are fixed, so we can calculate the number of time steps by rearanging the equation for N.
-
-            TVR = (1-r^N) / 1 - r  to make N the subject    N = (log(r-TVR(1-r)))/log(r) - 1
-
-            
-            The length of each time step will depend on how long we want the program to take to reduce the master volume to 0.
-            Let us call the time to reduce the master volume to 0, t.
-
-            t = alpha * N , where alpha is the length of each time step, and N is the number of time steps.
-
-            We can solve alpha be rearranging the equation.
-
-            */
-
-            float r = 1.01f;
-            int t = 5;
-
-
-
-            if(VolumeMixer.GetVolume(TargetID) * 100 < priorMasterVolume) //only increase the volume if it is less than what the volume was fading it out
-            {
-                float tv = priorMasterVolume;
-                //derrive N
-                int N = (int)Math.Round((Math.Log(r - tv * (1 - r)) / Math.Log(r)) - 1);
-
-                //derrive alpha
-                decimal alpha = decimal.Divide(t, N);
-                Console.WriteLine("N " + N);
-                Console.WriteLine("alpha " + alpha);
-
-                //increase the master volume
-                for (int i = 1; i <= N; i++)
-                {
-                    Thread.Sleep((int)Math.Round(alpha * (decimal)Math.Pow(10, 3)));
-                    float current_increase = (float)Math.Pow(r, i) / 100;
-                    VolumeMixer.ChangeVolume(TargetID, current_increase);
-                }
-            }
-
-
-
-        }
-
+    
         private void SaveProcess(ref ProcessInfo proc, string procName)
         {
             proc = FindProcess(procName);
--- Hush/VolumeMixer.cs
+++ Hush/VolumeMixer.cs
@@ -17,7 +17,6 @@ namespace Hush
         private static double threshold_noise = 0.00001;
         public static bool ProcessPlayingAudio(int pID)
         {
-            Console.WriteLine(pID);
             using (var sessionManager = GetDefaultAudioSessionManager2(DataFlow.Render))
             {
                 using (var sessionEnumerator = sessionManager.GetSessionEnumerator())
@@ -31,7 +30,7 @@ namespace Hush
                                 //Debug.WriteLine(session2.Process.ProcessName);
                                 if(pID == session2.ProcessID)
                                 {
-                                    Debug.WriteLine(audioMeterInformation.GetPeakValue());
+                                    //Debug.WriteLine(audioMeterInformation.GetPeakValue());
                                     if((double)audioMeterInformation.GetPeakValue() > threshold_noise)
                                     {
                                         return true;
@@ -131,7 +130,7 @@ namespace Hush
                                         {
                                             simpleVolume.MasterVolume = 0;
                                         }
-                                        else if(newVolume > 100)
+                                        else if(newVolume > 1)
                                         {
                                             simpleVolume.MasterVolume = 100;
                                         }
@@ -165,6 +164,157 @@ namespace Hush
         }
 
 
+        public static string FindMixerState(int speakerID, int targetID)
+        {
+            string mixState = "";
+
+            bool speakerOn = ProcessPlayingAudio(speakerID);
+            bool targetOn = ProcessPlayingAudio(targetID);
+
+
+            if (speakerOn && targetOn)
+            {
+                mixState =  "FadeOut";
+            }
+            else if (!speakerOn && targetOn)
+            {
+                mixState = "FadeIn";
+            }
+            return mixState;
+        }
+
+
+
+        public static void FadeOut(int SpeakerID, int TargetID) //method to reduce volume exponentially
+        {
+            /*
+                                                            ----- EXPLANATION -----
+
+             The method used here to fade out volume is to at fixed intervals, reduce the master volume by an exponential function.
+
+                                     The exponential function grows by these time steps being its exponent
+            More specifically  y = r^n , where n is the current time step, and y is the value to be subtracted from the master volume
+
+
+            The sum of an exponential function from 1 to N is (1-r^N) / 1 - r
+            We can set this equation equal to the current master volume
+
+            Now we know after N time intervals, if we subtract r^n, we will eventually reduce the master volume to 0.
+
+            The variables r, and the total volume reduction (TVR) i.e the 'current master volume', are fixed, so we can calculate the number of time steps by rearanging the equation for N.
+
+            TVR = (1-r^N) / 1 - r  to make N the subject    N = (log(r-TVR(1-r)))/log(r) - 1
+
+            
+            The length of each time step will depend on how long we want the program to take to reduce the master volume to 0.
+            Let us call the time to reduce the master volume to 0, t.
+
+            t = alpha * N , where alpha is the length of each time step, and N is the number of time steps.
+
+            We can solve alpha be rearranging the equation.
+
+            */
+
+
+
+            float r = 1.01f;
+            int t = 4;
+
+
+            float tvr = GetVolume(TargetID) * 100; //easier to work with values 0 to 100 and r values look nicer
+
+
+
+            //derrive N
+            int N = (int)Math.Floor((Math.Log(r - tvr * (1 - r)) / Math.Log(r)) - 1);
+            if(N != 0)
+            {
+                //Console.WriteLine("n " + N);
+                //derrive alpha
+                decimal alpha = decimal.Divide(t, N);
+                //Console.WriteLine("N " + N);
+                //Console.WriteLine("alpha " + alpha);
+
+                //reduce the master volume
+                for (int i = 1; i <= N; i++)
+                {
+                    Thread.Sleep((int)Math.Round(alpha * 1000));
+                    float current_reduction = -(float)Math.Pow(r, i) / 100; //return values back between 1 and 0 for actual reduction
+                    ChangeVolume(TargetID, current_reduction);
+
+
+
+
+                }
+            }
+
+
+
+        }
+
+
+        public static void FadeIn(int SpeakerID, int TargetID, float target_loud_volume) //method to reduce volume exponentially
+        {
+            /*
+                                                            ----- EXPLANATION -----
+
+             The method used here to fade out volume is to at fixed intervals, reduce the master volume by an exponential function.
+
+                                     The exponential function grows by these time steps being its exponent
+            More specifically  y = r^n , where n is the current time step, and y is the value to be subtracted from the master volume
+
+
+            The sum of an exponential function from 1 to N is (1-r^N) / 1 - r
+            We can set this equation equal to the current master volume
+
+            Now we know after N time intervals, if we subtract r^n, we will eventually reduce the master volume to 0.
+
+            The variables r, and the total volume reduction (TVR) i.e the 'current master volume', are fixed, so we can calculate the number of time steps by rearanging the equation for N.
+
+            TVR = (1-r^N) / 1 - r  to make N the subject    N = (log(r-TVR(1-r)))/log(r) - 1
+
+            
+            The length of each time step will depend on how long we want the program to take to reduce the master volume to 0.
+            Let us call the time to reduce the master volume to 0, t.
+
+            t = alpha * N , where alpha is the length of each time step, and N is the number of time steps.
+
+            We can solve alpha be rearranging the equation.
+
+            */
+
+            float r = 1.01f;
+            int t = 4;
+
+
+
+            if (GetVolume(TargetID) < target_loud_volume) //only increase the volume if it is less than what the volume was fading it out
+            {
+                float tv = target_loud_volume - GetVolume(TargetID);
+                //derrive N
+                tv *= 100;
+                int N = (int)Math.Round((Math.Log(r - tv * (1 - r)) / Math.Log(r)) - 1);
+                if (N != 0)
+                {
+                    //derrive alpha
+                    decimal alpha = decimal.Divide(t, N);
+                    //Console.WriteLine("N " + N);
+                    //Console.WriteLine("alpha " + alpha);
+
+                    //increase the master volume
+                    for (int i = 1; i <= N; i++)
+                    {
+                        Thread.Sleep((int)Math.Round(alpha * (decimal)Math.Pow(10, 3)));
+                        float current_increase = (float)Math.Pow(r, i) / 100;
+                        ChangeVolume(TargetID, current_increase);
+                    }
+                }
+
+            }
+
+
+
+        }
 
 
     }
